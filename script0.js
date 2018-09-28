9772fca931052713a8b35dfa5e936023d75f4d96
nikaalin <43650488+nikaalin@users.noreply.github.com>
2018-09-28 11:42:57 +0300

    Update script0.js

--- script0.js
+++ script0.js
@@ -1,7 +1,8 @@
 
 function validateY() {
     document.forms["form"].elements["Y"].onkeypress = function (event) {
-        if ((event.keyCode < 48 || event.keyCode > 57) && event.keyCode!==127 && event.keyCode!==46) {
+        if ((event.keyCode < 48 || event.keyCode > 57) && event.keyCode!==127 && event.keyCode!==46 && event.keyCode!==189) {
+            document.getElementById("errY").innerHTML = 'Не отчаивайтесь, на клавиатуре<br> не так уж много кнопок.';
             document.getElementById("errY").style.visibility = 'visible';
             return false;
         }
@@ -11,8 +12,10 @@ function validateY() {
 
 function validateForm(){
     onsubmit = function () {
-        if (!document.forms["form"].elements["Y"].value)
+        if ((!document.forms["form"].elements["Y"].value)||(document.forms["form"].elements["Y"].value<=-3)||(document.forms["form"].elements["Y"].value>=5)) {
+            document.getElementById("errY").innerHTML = 'Введите значение Y<br>в диапазоне (-3; 5)';
             document.getElementById("errY").style.visibility = 'visible';
+        }
         else {
             document.getElementById("errY").style.visibility = 'hidden';
         }
@@ -21,9 +24,8 @@ function validateForm(){
         else {
             document.getElementById("errR").style.visibility = 'hidden';
         }
-        if ((!document.forms["form"].elements["Y"].value)||(!document.forms["form"].elements["R"].value))
+        if ((!document.forms["form"].elements["Y"].value)||(!document.forms["form"].elements["R"].value)||document.getElementById("errY").style.visibility === 'visible')
             return false;
-        
-
+      
     }
 }
